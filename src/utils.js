e9eda01dee2ef54b054653d70ab2b72d01548619
SeemsRocky <rliao3613@gmail.com>
2020-01-21 12:39:57 -0500

    fetching current weather

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,23 @@
-const fetchWeather = cityName => {
-  const API_KEY = "<INSERT_API_KEY>";
+const fetchWeather = async (cityName) => {
+  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
   // TODO: fetch weather forecast from endpoint
   // from https://openweathermap.org/api
+  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${API_KEY}&units=imperial`
+  let dataObj = {}
+  await fetch(URL)
+  .then(res =>  res.json())
+  .then(data => {
+    const {cod} = data
+    if (cod==="404"){
+    dataObj['error']= 'Invalid city input'
+    } else {
+      dataObj = data 
+    }
+  })
+  .catch(error => {
+      dataObj['error']= 'Invalid city input'
+    })
+  return dataObj
 };
+
+export default fetchWeather;
