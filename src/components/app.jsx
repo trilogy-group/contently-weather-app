dd050925f03ae8639c5f78467c2da6d0eba46494
brianflynn <brianf@generalassemb.ly>
2020-02-23 16:59:47 -0500

    add switch units

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { ReactComponent as Logo } from "../images/sun.svg";
-import cloudy from '../images/cloudy.png';
-import rainy from '../images/rainy.png';
-import sunny from '../images/sunny.png';
 import "../App.css";
 import { getWeather, getForecast } from '../utils.js';
 
@@ -17,22 +14,25 @@ class App extends React.Component {
       iconLink: ""
     },
     forecast: [],
-    view: "forecast"
+    view: "current",
+    unit: "metric"
   }
 
   async componentDidMount() {
     if (document.location.pathname !== "/") {
       const cityName = document.location.pathname.slice(1).replace(/%20/g, " ")
+      const { unit } = this.state
       this.setState({
         cityName
       })
-      this.handleWeatherState(cityName)
-      this.handleForecastState(cityName)
+      this.handleWeatherState(cityName, unit)
+      this.handleForecastState(cityName, unit)
     }
   }
 
-  handleWeatherState = async (cityName) => {
-    const response = await getWeather(cityName)
+  handleWeatherState = async (cityName, unit) => {
+    const response = await getWeather(cityName, unit)
+    console.log(response)
     const { main, description, icon } = response.weather[0]
     const iconLink = `http://openweathermap.org/img/wn/${icon}@2x.png`
     const temp = Math.round(response.main.temp)
@@ -48,16 +48,9 @@ class App extends React.Component {
     })
   }
 
-  handleForecastState = async (cityName) => {
-    const response = await getForecast(cityName)
-
-    const forecastData = response.list
-    // const dt = response.list[3].dt
-    // var date = new Date(dt * 1000);
-    let options = {
-      weekday: 'long',
-      hour: 'numeric'
-    }
+  handleForecastState = async (cityName, unit) => {
+    const forecastData = await getForecast(cityName, unit)
+    const options = { weekday: 'long', hour: 'numeric' }
     const forecast = forecastData.map(forecast => {
       let dateTime = new Date(forecast.dt * 1000).toLocaleDateString('en-US', options)
       return {
@@ -69,7 +62,6 @@ class App extends React.Component {
         iconLink: `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
       }
     })
-    console.log(forecast)
     this.setState({
       forecast
     })
@@ -83,10 +75,29 @@ class App extends React.Component {
   }
 
   handleClick = () => {
-    const { cityName } = this.state
+    const { cityName, unit } = this.state
     document.location.pathname = cityName
-    this.handleWeatherState(cityName)
-    this.handleForecastState(cityName)
+    this.handleWeatherState(cityName, unit)
+    this.handleForecastState(cityName, unit)
+  }
+
+  handleView = (e) => {
+    e.preventDefault()
+    const view = e.target.id === "current-button" ? "current" : "forecast";
+    this.setState({
+      view
+    })
+  }
+
+  handleUnit = (e) => {
+    const { cityName } = this.state
+    const unitId = e.target.id
+    const unit = unitId === "F" ? "imperial" : "metric";
+    this.setState({
+      unit
+    })
+    this.handleWeatherState(cityName, unit)
+    this.handleForecastState(cityName, unit)
   }
 
   render() {
@@ -94,39 +105,71 @@ class App extends React.Component {
       cityName,
       currentWeather,
       forecast,
-      view
+      view,
+      unit
     } = this.state
 
     return (
       <div>
-        <h1><Logo />Weather</h1>
-        <input type="text" name="cityName" onChange={this.handleChange} placeholder="Enter City... " />
-        <button onClick={this.handleClick}>Get Weather</button><br />
-        <a href="#" onClick={this.handleCurrentWeather}>Current Weather</a>
-        <a href="#" onClick={this.handleForecast}>Forecast</a>
-        {
-          currentWeather.main && view === "current" &&
-          <div>
-            <h3>{cityName} Weather:</h3>
-            <img src={currentWeather.iconLink} />
-            <p>{currentWeather.description}</p>
-            <p>temp: {currentWeather.temp}&deg;F</p>
-            <p>wind: {currentWeather.windSpeed}mph</p>
+        <header>
+          <h1><Logo />Weather</h1>
+          <input type="text" name="cityName" onChange={this.handleChange} placeholder="Enter City... " />
+          <button onClick={this.handleClick}>Get Weather</button><br />
+          <div id="view-options" className={`${view}-link`} >
+            <a href="#" id="current-button" onClick={this.handleView}>Current</a>
+            <a href="#" id="forecast-button" onClick={this.handleView}>Forecast</a>
           </div>
-        }
-        {
-          view === "forecast" && forecast.map((forecast, index) => (
-            <div key={index}>
-              <p>{forecast.dateTime}</p>
-              <img src={forecast.iconLink} />
-              <p>{forecast.description}</p>
-              <p>temp: {forecast.temp}&deg;F</p>
-              <p>wind: {forecast.windSpeed}mph</p>
-              <hr />
+          <button id="F" onClick={this.handleUnit}>&deg;F</button>
+          <button id="C" onClick={this.handleUnit}>&deg;C</button>
+        </header>
+        <main>
+          {
+            currentWeather.main && view === "current" &&
+            <div id="current">
+              <h3>{cityName} Weather:</h3>
+              <img src={currentWeather.iconLink} />
+              <p>{currentWeather.description}</p>
+              {
+                unit === "imperial" ?
+                  <>
+                    <p>temp: {currentWeather.temp}&deg;F</p>
+                    <p>wind: {currentWeather.windSpeed}mph</p>
+                  </> :
+                  <>
+                    <p>temp: {currentWeather.temp}&deg;C</p>
+                    <p>wind: {currentWeather.windSpeed}kph</p>
+                  </>
+              }
             </div>
-          ))
-        }
-      </div>
+          }
+
+          {
+            view === "forecast" && cityName &&
+            <div id="forecast">
+              <h3>{cityName} Forecast:</h3>
+              {
+                forecast.map((forecast, index) => (
+                  <div key={index} className="forecast-segment">
+                    <p>{forecast.dateTime}</p>
+                    <img src={forecast.iconLink} />
+                    <p>{forecast.description}</p>
+                    {
+                      unit === "imperial" ?
+                        <>
+                          <p>temp: {currentWeather.temp}&deg;F</p>
+                          <p>wind: {currentWeather.windSpeed}mph</p>
+                        </> :
+                        <>
+                          <p>temp: {currentWeather.temp}&deg;C</p>
+                          <p>wind: {currentWeather.windSpeed}kph</p>
+                        </>
+                    }
+                  </div>
+                ))}
+            </div>
+          }
+        </main>
+      </div >
     )
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const API_KEY = "d29e9cce44012ae03806fcd9edc39a4e"
 
-const getWeather = async cityName => {
-  const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=imperial`)
+const getWeather = async (cityName, unit) => {
+  const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=${unit}`)
   return response.data
 };
 
-const getForecast = async cityName => {
-  const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&units=imperial`)
-  return response.data
+const getForecast = async (cityName, unit) => {
+  const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&units=${unit}`)
+  return response.data.list
 };
 
 export {
