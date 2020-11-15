468b30bcc965efd2ad95779d62f9d46b18be14ba
brianblattner <brian.blattner@sweq.tv>
2020-11-15 16:04:23 -0600

    Init commit. Adds and configures tailwindcss

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { ReactComponent as Logo } from "../images/sun.svg";
 import "../App.css";
 
-function App() {
-  return <h1>Weather</h1>;
+const App = () => {
+  return (
+    <h1 className="max-w-7xl">Weather</h1>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 
+import './tailwind.output.css';
+
 ReactDOM.render(<App />, document.getElementById("root"));
