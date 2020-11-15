6f84e3d17ca0728150ca085983f5cbf93970ebb1
brianblattner <brian.blattner@sweq.tv>
2020-11-15 16:21:36 -0600

    Creates basic routes and splits TopNav into separate component

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="max-w-3xl mt-10 m-auto text-center">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Home;
