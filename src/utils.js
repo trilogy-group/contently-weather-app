325c93cf9d6e4fdba76e3ce68f110be1982556ee
Mimi Scott <mimiscott94@gmail.com>
2020-12-07 08:51:38 -0500

    Added needed packages, created utils fetchWeather function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,22 @@
-const fetchWeather = cityName => {
-  const API_KEY = "<INSERT_API_KEY>";
-  // TODO: fetch weather forecast from endpoint
-  // from https://openweathermap.org/api
+const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+
+export const fetchWeather = (cityName, units) => {
+  const lookup = cityName.toLowerCase();
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${lookup}&appid=${API_KEY}&units=${units}`;
+  return fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      if (res.cod !== 200 ) {
+        throw new Error();
+      }
+      const lat = res.coord.lat;
+      const long = res.coord.lon;
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${API_KEY}&units=${units}`;
+      return fetch(url);
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (!res.current) throw new Error();
+      return res;
+    });
 };
